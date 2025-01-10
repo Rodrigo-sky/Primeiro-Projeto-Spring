0aa2c7ce1a677dc08a64815abfd48a8165f694bc
Rodrigo <rtrs.eu@gmail.com>
2025-01-09 23:28:14 -0300

    feat: OrderStatus enum

--- src/main/java/com/rodrigoteixeira/primeiro_projeto_spring/config/TestConfig.java
+++ src/main/java/com/rodrigoteixeira/primeiro_projeto_spring/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.rodrigoteixeira.primeiro_projeto_spring.config;
 
 import com.rodrigoteixeira.primeiro_projeto_spring.entities.Order;
 import com.rodrigoteixeira.primeiro_projeto_spring.entities.User;
+import com.rodrigoteixeira.primeiro_projeto_spring.entities.enums.OrderStatus;
 import com.rodrigoteixeira.primeiro_projeto_spring.repositories.OrderRepository;
 import com.rodrigoteixeira.primeiro_projeto_spring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = User.builder().nome("Maria Brown").email("maria@gmail.com").phone("988888888").password("123456").build();
         User u2 = User.builder().nome("Alex Green").email("alex@gmail.com").phone("977777777").password("123456").build();
 
-        Order o1 = Order.builder().moment(Instant.parse("2019-06-20T19:53:07Z")).client(u1).build();
-        Order o2 = Order.builder().moment(Instant.parse("2019-07-21T03:42:10Z")).client(u2).build();
-        Order o3 = Order.builder().moment(Instant.parse("2019-07-22T15:21:22Z")).client(u1).build();
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
--- src/main/java/com/rodrigoteixeira/primeiro_projeto_spring/entities/Order.java
+++ src/main/java/com/rodrigoteixeira/primeiro_projeto_spring/entities/Order.java
@@ -1,13 +1,8 @@
 package com.rodrigoteixeira.primeiro_projeto_spring.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import com.rodrigoteixeira.primeiro_projeto_spring.entities.enums.OrderStatus;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -35,8 +30,27 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getValue();
+        }
+    }
+
+    @Builder
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
 }
